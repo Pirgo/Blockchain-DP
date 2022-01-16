37a04704e0012c26163a777ecf64659b32f5b84f
Michał <MCjasiux@gmail.com>
2022-01-16 17:45:11 +0100

    synchronizacja
    peery będą przyjmować powszechne blockchainy, poprawne bloki i w tej chwili wszystkie transakcje

--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -1,25 +1,25 @@
 const Block = require('./block');
 
-class Blockchain{
-    constructor(){
+class Blockchain {
+    constructor() {
         this.chain = [Block.genesis()];
     }
 
-    static isValidChain(chain){
-        if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
+    static isValidChain(chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
             return false;
-        
-        for(let i = 1 ; i<chain.length; i++){
+
+        for (let i = 1; i < chain.length; i++) {
             const block = chain[i];
-            const lastBlock = chain[i-1];
-            if((block.lastHash !== lastBlock.hash) || (block.hash !== Block.blockHash(block)))
+            const lastBlock = chain[i - 1];
+            if ((block.lastHash !== lastBlock.hash) || (block.hash !== Block.blockHash(block)))
                 return false;
         }
 
-        return true;    
+        return true;
     }
 
-    getGenesis(){
+    getGenesis() {
         return this.chain[0];
     }
 
@@ -44,29 +44,32 @@ class Blockchain{
     //     }
     //     return null;
     // }
+    appendBlock(block) { //dodanie bez kopania
+        //todo poprawność
 
-    addBlock(data){
+    }
+    addBlock(data) {
         const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
         this.chain.push(block);
         return block;
     }
 
     //TODO: rozwiązać problem kiedy otrzymujemy chainy od dwoch nodeów jednocześnie
-    replaceChain(newChain){
+    replaceChain(newChain) {
         console.log(newChain);
-        //TODO: tu wyodrębnic czy otrzymujemy dluzzszy chain czy taki sam
-        if(newChain.length <= this.chain.length){
-            console.log("Recieved chain is not longer than the current chain");
-            return;
-        }else if(!Blockchain.isValidChain(newChain)){
-            console.log("Recieved chain is invalid");
-            return;
-        }
-        
+        //: tu wyodrębnic czy otrzymujemy dluzzszy chain czy taki sam - w p2p-server
+        // if (newChain.length <= this.chain.length) {
+        //     console.log("Recieved chain is not longer than the current chain");
+        //     return;
+        // } else if (!Blockchain.isValidChain(newChain)) {
+        //     console.log("Recieved chain is invalid");
+        //     return;
+        // }
+
         console.log("Replacing the current chain with new chain");
-        this.chain = newChain; 
+        this.chain = newChain.chain;
     }
-    
+
 }
 
 module.exports = Blockchain;
--- stun/server.js
+++ stun/server.js
@@ -1,19 +1,29 @@
 const PORT = 5001;
-const HOST = "172.104.240.26";
-const TIME = 120000 //2 minuty
+const HOST = '192.168.100.52'//"172.104.240.26";
+const TIME = 120 //2 minuty
 var dgram = require('dgram');
 const { parse } = require('querystring');
 
 const server = dgram.createSocket('udp4');
 
 const stunTable = new Map()
-//stunTable.set("192.168.100.52", { "port": PORT, timeout })
 
-function timeoutHandler(key) {
-    return setTimeout(() => {
-        stunTable.delete(key)   //wyrejestrowanie peera z tablicy
-        notify()
-    }, TIME)
+function timeoutHandler() {
+    let deadList = []
+    stunTable.forEach((val, key, map) => {
+
+        if (val.ttl <= 0) {
+            deadList.push(key)
+        } else {
+            val.ttl -= 10
+        }
+
+    })
+    deadList.forEach(element => {
+        stunTable.delete(element)
+    });
+    notify()
+
 }
 function send(msg, addr, port) {
     msgStr = JSON.stringify(msg)
@@ -31,13 +41,14 @@ function notify() {  //rozesłanie tablicy stun wszystkim peerom
     })
     stunTable.forEach((val, key, map) => {
         let filtered = toSend.filter((el) => { return el.addr != val.addr || el.port != val.port }) //nie wysyłamy peerowi jego adresu
-        if (filtered.length > 0)
-            send({ "type": "table", "table": filtered }, val.addr, val.port)
+        //if (filtered.length > 0)
+        send({ "type": "table", "table": filtered }, val.addr, val.port)
     })
 }
 server.on('listening', function () {
     var address = server.address();
     console.log('UDP Server listening on ' + address.address + ':' + address.port);
+    setInterval(timeoutHandler, 10000);
 });
 
 server.on('message', function (message, remote) {
@@ -55,14 +66,17 @@ server.on('message', function (message, remote) {
     switch (parsedMsg.type) {
         case "register":    //rejestracja adresu i portu
             //console.log("registering new peer")
-            stunTable.set(pair, { addr: remote.address, port: remote.port, timeout: timeoutHandler(pair) })
+            stunTable.set(pair, { addr: remote.address, port: remote.port, ttl: TIME })
             notify()
             break;
         case "alive":   //odpowiedź na "ping" - peer uczestniczy w sieci
-
-            let peer = stunTable.get(pair)
-            clearTimeout(peer.timeout)
-            peer.timeout = timeoutHandler(pair) //odnowienie dzierżawy w tablicy peerów
+            try {
+                let peer = stunTable.get(pair)
+                peer.ttl = TIME
+            } catch (e) {
+                stunTable.set(pair, { addr: remote.address, port: remote.port, ttl: TIE }) //jeśli peer odnawia uczestnictwo, ale serwer go usunął. doda go na nowo
+                notify()
+            }
             break;
         case "ask":     //pytanie o adresy:porty innych peerów
             console.warn("type:'ask' deprecated")
@@ -74,7 +88,7 @@ server.on('message', function (message, remote) {
             console.warn("unrecognized message type")
             break;
     }
-   // console.log(stunTable)
+    // console.log(stunTable)
 
 });
 
